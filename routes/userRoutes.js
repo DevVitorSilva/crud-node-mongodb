1f6585d81c5cdf3a1c584f14f08acc738f0cbe5d
João Vitor <joao@joaovitor.com>
2023-06-26 08:15:25 -0300

    Na rota get /people foi modificada para buscar todos os usuários no banco de dados. Foi criada a rota post /register para registrar o usuário.

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,16 +1,35 @@
-import { Router } from "express";
+import e, { Router } from "express";
 import { User } from "../models/UserModel.js";
 
 const routes = Router();
 
-routes.get("/people", (req, res) => {
-    const user = new User({name: "John Doe"})
-    user.save().then((doc) => {
-        console.log(doc)
-    }).catch((err) => {
-        console.log(`user no saved: ${err}`)
-    })
-    res.status(200).json({ message: "Hello from express" });
+routes.get("/people", async (req, res) => {
+    try {
+        const allNames = await User.find({});
+        res.status(200).json({ allNames });
+    } catch (err) {
+        console.log(`getAllNames error: ${err}`);
+        res.status(500).json({ message: "Internal error server" });
+    }
+    
+    
+});
+
+routes.post("/register", e.json(), async (req, res) => {
+    const { name } = req.body;
+    const user = new User({name});
+
+    if(!name){
+        return res.status(400).json({ message: "Insira o nome" });
+    }
+
+    try {
+        await user.save()
+        res.status(201).json({user});
+    } catch (err) {
+        console.log(`UserSaved error: ${err}`);
+        res.status(500).json({ message: "Internal server error" });
+    }   
 });
 
 export{
